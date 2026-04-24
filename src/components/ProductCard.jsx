c4f03123d912ab64b68b41d7dbb64217373d1fae
Mjeppo <mjeppo@live.nl>
2026-04-24 17:22:23 +0200

    image alt toegevoegd

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
 
 export default function ProductCard({ product }) {
-     
-    return (
-        <div className="product-card">
-    <img src={product.image} />
-    <div className="product-card-content">
-      <h3 className="product-card-name">{product.name}</h3>
-      <p className="product-card-price">€ {product.price}</p>
-      <div className="product-card-actions">
-        <Link className="btn btn-secondary">Bekijk details</Link>
-        <button className="btn btn-primary">Voeg toe aan winkelwagen</button>
+  return (
+    <div className="product-card">
+      <img src={product.image} className="product-card-image" alt={product.name} />
+      <div className="product-card-content">
+        <h3 className="product-card-name">{product.name}</h3>
+        <p className="product-card-price">€ {product.price}</p>
+        <div className="product-card-actions">
+          <Link className="btn btn-secondary">Bekijk details</Link>
+          <button className="btn btn-primary">Voeg toe aan winkelwagen</button>
+        </div>
       </div>
     </div>
-  </div>)
+  );
 }
