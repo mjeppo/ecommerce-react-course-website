9388a9314ba0fadcf1e3c1bbc7892d62f8a78918
Mjeppo <mjeppo@live.nl>
2026-04-25 11:16:11 +0200

    Added authentication

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,22 @@ import Auth from "./pages/Auth";
 import Home from "./pages/Home";
 import Checkout from "./pages/Checkout";
 import Navbar from "./components/Navbar";
+import AuthProvider from "./context/AuthContext";
 
 function App() {
 
-  return <div className="app">
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/auth" element={<Auth />} />
-      <Route path="/checkout" element={<Checkout /> } />
-    </Routes>
-  </div>;
+  return (
+    <AuthProvider>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </div>
+    </AuthProvider>
+  );
 }
 
 export default App;
